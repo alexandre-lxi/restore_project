59c4f981fdcf349f6ea3ad63b7b0c0690a608ba0
Alexandre Laidin <alexandre@lxi.eu.com>
2017-08-20 11:56:30 +0200

    commit

--- restoreVideo.php
+++ restoreVideo.php
@@ -9,13 +9,13 @@
 $VALEUR_hote='prod.kwk.eu.com';
 $VALEUR_port='3306';
 $VALEUR_nom_bd='total-refontedam';
-$VALEUR_user='BdTotal$4';
-$VALEUR_mot_de_passe='RvaBD$-R5';
+$VALEUR_user='alaidin';
+$VALEUR_mot_de_passe='alaidin';
 
 try{
     $pdo = new PDO('mysql:host='.$VALEUR_hote.';port='.$VALEUR_port.';dbname='.$VALEUR_nom_bd, $VALEUR_user, $VALEUR_mot_de_passe);
 }catch( PDOException $Exception ) {
     // PHP Fatal Error. Second Argument Has To Be An Integer, But PDOException::getCode Returns A
     // String.
-    echo $Exception->getMessage( ).' : '.$Exception->getCode( ) );
+    echo $Exception->getMessage( ).' : '.$Exception->getCode( );
 }
