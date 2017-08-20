196f78966c57666e2dbf6ef57bf43e421ab6fb45
Alexandre Laidin <alexandre@lxi.eu.com>
2017-08-20 12:35:34 +0200

    commit

--- restoreVideo.php
+++ restoreVideo.php
@@ -19,3 +19,39 @@ try{
     // String.
     echo $Exception->getMessage( ).' : '.$Exception->getCode( );
 }
+
+//$dir    = '/home/ubuntu/';
+$dirsource = '/home/alex/Documents/IRIS/Clients/kwk/total/tmp/';
+$dest = '/home/alex/Documents/IRIS/Clients/kwk/total/tmp/dest/';
+$files = scandir($dirsource);
+
+foreach ($files as $file){
+    if (!is_dir($file)) {
+        $fileSize = filesize($dirsource.$file);
+        $fileDet = explode('.', $file);
+        $fileExt = $fileDet[1];
+
+        echo $file.':'.$fileExt.':'.$fileSize."\n";
+
+        $sql = "SELECT s_path
+            FROM `total-refontedam`.image_file imf, `total-refontedam`.container co, `total-refontedam`.image_infofr info
+            WHERE co.i_autocode = imf.i_foreigncode
+              AND co.i_autocode = info.i_foreigncode
+              AND right(co.s_reference,3) = '".$fileExt."'
+              AND i_filesize = ".$fileSize;
+
+        $rows = $pdo->query($sql);
+
+        if ($rows->rowCount() == 1) {
+            $row = $rows->fetchAll();
+            print_r($row);
+        } elseif ($rows->rowCount() == 0) {
+            echo "0 ligne";
+        } else {
+            $row = $rows->fetchAll();
+            print_r($row);
+        }
+
+        echo "\n";
+    }
+}
