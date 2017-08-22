465021e20868b1e49c1c6f0e754d484c5ccf3cc0
Alexandre Laidin <alexandre@lxi.eu.com>
2017-08-23 01:39:56 +0200

    c

---
+++ verifVideo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<video width="400" controls>
+    <?php echo '<source src="pictures/"'._GET['vid'].' type="video/mp4">'; ?>
+</video>
+</body>
+</html>
