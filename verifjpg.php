fd4eaaaa8cc99130b47da905df171d9bf1eadb36
Alexandre Laidin <alexandre@lxi.eu.com>
2017-08-23 19:01:09 +0200

    m

--- verifjpg.php
+++ verifjpg.php
@@ -9,30 +9,46 @@
 include 'iptc.php';
 
 echo "<html>";
-echo "<body>";
+
+?>
+<html>
+<head>
+    <style>
+        table {
+            border-collapse: collapse; /* Les bordures du tableau seront collées (plus joli) */
+        }
+
+        td {
+            border: 1px solid black;
+        }
+    </style>
+
+
+</head>
+<body>
+<?php
 
 $cds = array("005" => "ip_name",
-            "010" => "ip_urgency",
-            "015" => "ip_category",
-            "020" => "ip_supcategories",
-            "025" => "ip_keywords",
-            "040" => "ip_instruction",
-            "055" => "ip_created",
-            "080" => "ip_byline",
-            "085" => "ip_bylinetitle",
-            "090" => "ip_city",
-            "095" => "ip_state",
-            "100" => "ip_country_code",
-            "101" => "ip_country",
-            "103" => "ip_reference",
-            "105" => "ip_headline",
-            "110" => "ip_credits",
-            "115" => "ip_source",
-            "116" => "ip_copyright",
-            "120" => "ip_caption",
-            "121" => "ip_captionwriter2",
-            "122" => "ip_captionwriter");
-
+    "010" => "ip_urgency",
+    "015" => "ip_category",
+    "020" => "ip_supcategories",
+    "025" => "ip_keywords",
+    "040" => "ip_instruction",
+    "055" => "ip_created",
+    "080" => "ip_byline",
+    "085" => "ip_bylinetitle",
+    "090" => "ip_city",
+    "095" => "ip_state",
+    "100" => "ip_country_code",
+    "101" => "ip_country",
+    "103" => "ip_reference",
+    "105" => "ip_headline",
+    "110" => "ip_credits",
+    "115" => "ip_source",
+    "116" => "ip_copyright",
+    "120" => "ip_caption",
+    "121" => "ip_captionwriter2",
+    "122" => "ip_captionwriter");
 
 $fname = 'pictures/'.$_GET['img'];
 $size = getimagesize($fname, $info);
@@ -41,20 +57,21 @@ $iptc = new iptc();
 $iptc->setImg($fname);
 
 $liptc = $iptc->readIPTC();
+?>
+<table>
+    <?php
+    foreach ($cds as $cd) {
+        ?>
+        <tr>
+            <td><?php echo $cd; ?></td>
+            <td><?php echo $liptc[$cd]; ?></td>
+        </tr>
 
-echo "<table>";
-
-foreach ($cds as $cd){
+        <?php
+    }
     ?>
-<tr>
-    <td><?php echo $cd; ?></td>
-    <td><?php echo $liptc[$cd]; ?></td>
-</tr>
 
-<?php
-}
+</table>
 
-echo "</table>";
-
-echo "</body>";
-echo "</html>";
+</body>
+</html>
