e0f9a0d5cf51da66b4454715b51d1267a9f9e98f
Alexandre Laidin <alexandre@lxi.eu.com>
2017-08-23 18:57:50 +0200

    m

--- iptc.php
+++ iptc.php
@@ -18,77 +18,28 @@ class iptc
 
     function setImg($cheminImg)
     {
-        // Inititalisations
-
-        // Les valeurs IPTC pouvant être manipulées
-        $this->h_codesIptc = array("005" => "Nom de l'objet",
-            "007" => "Statut Éditorial",
-            "010" => "Priorité",
-            "015" => "Catégorie",
-            "020" => "Catégorie Supplémentaire",
-            "022" => "Identificateur",
-            "025" => "Mots-Clés",
-            "030" => "Date de disponibilité",
-            "035" => "Heure de disponibilité",
-            "040" => "Instructions spéciales",
-            "045" => "Service de référence",
-            "047" => "Date de référence",
-            "050" => "Numéro de référence",
-            "055" => "Created Date",
-            "060" => "Created Time",
-            "065" => "Originating Program",
-            "070" => "Program Version",
-            "075" => "Object Cycle",
-            "080" => "Byline",
-            "085" => "Byline Title",
-            "090" => "City",
-            "095" => "Province State",
-            "100" => "Country Code",
-            "101" => "Country",
-            "103" => "Original Transmission Reference",
-            "105" => "Headline",
-            "110" => "Credit",
-            "115" => "Source",
-            "116" => "Copyright String",
-            "120" => "Caption",
-            "121" => "Local Caption",
-            "122" => "Local Caption");
-
-        $this->h_codesIptc2 = array("005" => "s_objectname",
-            "007" => "Statut Éditorial",
-            "010" => "i_urgency",
-            "015" => "s_category",
-            "020" => "s_supcategories",
-            "022" => "Identificateur",
-            "025" => "s_keywords",
-            "030" => "Date de disponibilité",
-            "035" => "Heure de disponibilité",
-            "040" => "s_instruction",
-            "045" => "Service de référence",
-            "047" => "Date de référence",
-            "050" => "Numéro de référence",
-            "055" => "d_created",
-            "060" => "Created Time",
-            "065" => "Originating Program",
-            "070" => "Program Version",
-            "075" => "Object Cycle",
-            "080" => "s_byline",
-            "085" => "s_bylinetitle",
-            "090" => "s_city",
-            "095" => "s_state",
-            "100" => "Country Code",
-            "101" => "s_country",
-            "103" => "s_reference",
-            "105" => "s_headline",
-            "110" => "s_credits",
-            "115" => "s_source",
-            "116" => "s_copyright",
-            "120" => "s_caption",
-            "121" => "s_captionwriter2",
-            "122" => "s_captionwriter");
+        $this->h_codesIptc2 = array("005" => "ip_name",
+            "010" => "ip_urgency",
+            "015" => "ip_category",
+            "020" => "ip_supcategories",
+            "025" => "ip_keywords",
+            "040" => "ip_instruction",
+            "055" => "ip_created",
+            "080" => "ip_byline",
+            "085" => "ip_bylinetitle",
+            "090" => "ip_city",
+            "095" => "ip_state",
+            "100" => "ip_country_code",
+            "101" => "ip_country",
+            "103" => "ip_reference",
+            "105" => "ip_headline",
+            "110" => "ip_credits",
+            "115" => "ip_source",
+            "116" => "ip_copyright",
+            "120" => "ip_caption",
+            "121" => "ip_captionwriter2",
+            "122" => "ip_captionwriter");
         $this->h_fieldIptc = array_flip($this->h_codesIptc2);
-
-        // On enregistre le chemin de l'image à traiter
         $this->h_cheminImg = $cheminImg;
 
         // On extrait les données encodées de l'iptc
@@ -98,26 +49,6 @@ class iptc
         else
             $this->h_iptcData = false;
     }
-
-
-    function fct_lireIPTC()
-    {
-        $tblIPTC = iptcparse($this->h_iptcData);
-
-        while ((is_array($tblIPTC)) && (list($codeIPTC, $valeurIPTC) = each($tblIPTC))) {
-            $codeIPTC = str_replace("2#", "", $codeIPTC);
-
-            if (($codeIPTC != "000") && ($codeIPTC != "140")) {
-                while (list($index,) = each($valeurIPTC)) {
-                    $lesIptc[$codeIPTC] .= $valeurIPTC[$index].$retourLigne;
-                    $retourLigne = "\n";
-                }
-            }
-        }
-
-        if (is_array($lesIptc)) return $lesIptc; else return false;
-    }
-
 
     function readIPTC()
     {
@@ -152,8 +83,6 @@ class iptc
         }
     }
 
-    /* FIN FONCTION fct_lireIPTC(); */
-
     function _getIptcLabel($code)
     {
         if (isset($this->h_codesIptc2[$code]))
@@ -162,124 +91,4 @@ class iptc
             return "";
         }
     }
-
-    /*
-
--------------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-    INFOS SUR LA FONCTION
-
-        ROLE : écrit des IPTC dans le fichier image
-        FONCTION : fct_ecrireIPTC()
-        DESCRIPTION DES PARAMETRES :
-            -   $tblIPTC_util 		= 	(tableau associatif) contient les codes des champs IPTC à modifier associés leur valeur
-            -	$cheminImgAModifier =	(chaine) stocke le chemin de l'image dont l'IPTC est à modifier ; s'il est null
-                                                 le chemin sera celui contenu dans '$this -> h_cheminImg'
-        TYPE RETOURNE : booléen
-
-    FIN INFOS SUR LA FONCTION */
-
-    function fct_ecrireIPTC($tblIPTC_util, $cheminImgAModifier = "")
-    {
-
-        // La tableau devant contenir des IPTC est vide ou n'est pas un tableau associatif
-        if ((empty($tblIPTC_util)) || (!is_array($tblIPTC_util))) return false;
-
-        // Si le chemin de l'image à modifier est vide alors on lui spécifie le chemin par défaut
-        if (empty($cheminImgAModifier)) $cheminImgAModifier = $this->h_cheminImg;
-
-        // On récupère l'IPTC du fichier image courant
-        $tblIPTC_old = iptcparse($this->h_iptcData);
-
-        // On prélève le tableau contenant les codes et les valeurs des IPTC de la photo
-        while (list($codeIPTC, $codeLibIPTC) = each($this->h_codesIptc)) {
-
-            // On teste si les données originelles correspondant au code en cours sont présents
-            if (is_array($tblIPTC_old["2#".$codeIPTC])) $valIPTC_new = $tblIPTC_old["2#".$codeIPTC];
-            else $valIPTC_new = array();
-
-            // On remplace les valeurs des IPTC demandées
-            if (is_array($tblIPTC_util[$codeIPTC])) {
-                if (count($tblIPTC_util[$codeIPTC])) $valIPTC_new = $tblIPTC_util[$codeIPTC];
-            } else {
-
-                $val = trim(strval($tblIPTC_util[$codeIPTC]));
-                if (strlen($val)) $valIPTC_new[0] = $val;
-            }
-
-            // On crée un nouveau iptcData à partir de '$tblIPTC_new' qui contient le code et la valeur de l'IPTC
-            foreach ($valIPTC_new as $val) {
-                $iptcData_new .= $this->fct_iptcMaketag(2, $codeIPTC, $val);
-            }
-        }
-
-        /*	A partir du nouveau iptcData contenu dans '$iptcData_new' on crée grâce à la fonction 'iptcembed()'
-            le contenu binaire du fichier image avec le nouveau IPTC inclu */
-        $contenuImage = iptcembed($iptcData_new, $this->h_cheminImg);
-
-        // Ecriture dans le fichier image
-        $idFichier = fopen($cheminImgAModifier, "wb");
-        fwrite($idFichier, $contenuImage);
-        fclose($idFichier);
-
-        return true;
-    }
-
-    /* FIN FONCTION fct_ecrireIPTC();
-
-
-
-
-
-
-
-
--------------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-    INFOS SUR LA FONCTION
-
-        ROLE : permet de transformer une valeur de d'IPTC (code + valeur) en iptcData
-        AUTEUR : Thies C. Arntzen
-        FONCTION : fct_iptcMaketag($rec, $dat, $val)
-        DESCRIPTION DES PARAMETRES :
-            -   $rec = (entier) toujours à mettre à 2
-            -	$dat = (chaine) le code de l'IPTC (de type '110' et non '2#110')
-            -	$val = (chaine) la valeur de l'IPTC
-        TYPE RETOURNE : booléen
-
-    FIN INFOS SUR LA FONCTION */
-
-    function fct_iptcMaketag($rec, $dat, $val)
-    {
-        $len = strlen($val);
-        if ($len < 0x8000)
-            return chr(0x1c).chr($rec).chr($dat).
-                chr($len >> 8).
-                chr($len & 0xff).
-                $val;
-        else
-            return chr(0x1c).chr($rec).chr($dat).
-                chr(0x80).chr(0x04).
-                chr(($len >> 24) & 0xff).
-                chr(($len >> 16) & 0xff).
-                chr(($len >> 8) & 0xff).
-                chr(($len) & 0xff).
-                $val;
-    }
-
-    // FIN FONCTION fct_iptcMaketag();
 }
--- verifjpg.php
+++ verifjpg.php
@@ -11,6 +11,27 @@ include 'iptc.php';
 echo "<html>";
 echo "<body>";
 
+$cds = array("005" => "ip_name",
+            "010" => "ip_urgency",
+            "015" => "ip_category",
+            "020" => "ip_supcategories",
+            "025" => "ip_keywords",
+            "040" => "ip_instruction",
+            "055" => "ip_created",
+            "080" => "ip_byline",
+            "085" => "ip_bylinetitle",
+            "090" => "ip_city",
+            "095" => "ip_state",
+            "100" => "ip_country_code",
+            "101" => "ip_country",
+            "103" => "ip_reference",
+            "105" => "ip_headline",
+            "110" => "ip_credits",
+            "115" => "ip_source",
+            "116" => "ip_copyright",
+            "120" => "ip_caption",
+            "121" => "ip_captionwriter2",
+            "122" => "ip_captionwriter");
 
 
 $fname = 'pictures/'.$_GET['img'];
@@ -21,7 +42,19 @@ $iptc->setImg($fname);
 
 $liptc = $iptc->readIPTC();
 
-print_r($liptc);
+echo "<table>";
+
+foreach ($cds as $cd){
+    ?>
+<tr>
+    <td><?php echo $cd; ?></td>
+    <td><?php echo $liptc[$cd]; ?></td>
+</tr>
+
+<?php
+}
+
+echo "</table>";
 
 echo "</body>";
 echo "</html>";
