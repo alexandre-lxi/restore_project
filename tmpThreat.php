1eff007b6466beeed4a7544f6e945dbfa56297a6
Alexandre Laidin <alexandre@lxi.eu.com>
2017-08-26 14:58:40 +0200

    m

--- tmpThreat.php
+++ tmpThreat.php
@@ -73,29 +73,23 @@ function _get_video_attributes($video, $ffmpeg) {
     $command = $ffmpeg . ' -i ' . $video . ' -vstats 2>&1';
     $output = shell_exec($command);
 
-    $regex_sizes = "/Video: ([^,]*), ([^,]*), ([0-9]{1,4})x([0-9]{1,4})/"; // or : $regex_sizes = "/Video: ([^\r\n]*), ([^,]*), ([0-9]{1,4})x([0-9]{1,4})/"; (code from @1owk3y)
-    if (preg_match($regex_sizes, $output, $regs)) {
-        $codec = $regs [1] ? $regs [1] : null;
-        $width = $regs [3] ? $regs [3] : null;
-        $height = $regs [4] ? $regs [4] : null;
-    }
-
     $regex_duration = "/Duration: ([0-9]{1,2}):([0-9]{1,2}):([0-9]{1,2}).([0-9]{1,2})/";
     if (preg_match($regex_duration, $output, $regs)) {
         $hours = $regs [1] ? $regs [1] : null;
         $mins = $regs [2] ? $regs [2] : null;
         $secs = $regs [3] ? $regs [3] : null;
         $ms = $regs [4] ? $regs [4] : null;
-    }
 
-    return array('codec' => $codec,
-        'width' => $width,
-        'height' => $height,
-        'hours' => $hours,
-        'mins' => $mins,
-        'secs' => $secs,
-        'ms' => $ms
-    );
+        return array(
+            'hours' => $hours,
+            'mins' => $mins,
+            'secs' => $secs,
+            'ms' => $ms
+        );
+    }
+    else
+        return false;
+
 }
 
 function _human_filesize($bytes, $decimals = 2) {
