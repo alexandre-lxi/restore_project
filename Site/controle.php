f00676a026ca21d51eaa2647ae5a1ea1522f6b9f
Alexandre Laidin <alexandre@lxi.eu.com>
2017-09-07 21:26:24 +0200

    m

--- Site/controle.php
+++ Site/controle.php
@@ -13,8 +13,7 @@ try{
             from restore_file_co_analyse2
             where is_restored <> 5
             and co_code <> 1
-            and RAND() > 0.9
-            and rf_code = 23340
+            and RAND() > 0.9            
             and co_code in (select i_autocode from container where b_isintrash <> 0) 
             group by co_code 
             having count(*)>1
