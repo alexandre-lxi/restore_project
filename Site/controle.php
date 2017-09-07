230b9d6b28d29a6dd7f5f63679fc46fc69ab2938
Alexandre Laidin <alexandre@lxi.eu.com>
2017-09-07 19:08:23 +0200

    m

---
+++ Site/controle.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Controle images</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div id="entete">
+    En tête
+</div>
+
+<div id="main">
+    <div id="menu">
+        Menu
+    </div>
+
+    <div id="contenu">
+        Contenu
+    </div>
+</div>
+
+<div id="footer">
+    Pied de Page
+</div>
+
+</body>
+</html>
