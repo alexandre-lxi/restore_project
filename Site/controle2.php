cce5f6e88f6b407562b8c580f810c8c311af3505
Alexandre Laidin <alexandre@lxi.eu.com>
2017-09-09 13:15:44 +0200

    m

--- Site/controle2.php
+++ Site/controle2.php
@@ -8,6 +8,45 @@ $VALEUR_mot_de_passe = 'alaidin';
 
 $name = (isset($_GET['name'])?$_GET['name']:'');
 
+function findBySizes($width, $height)
+{
+    $VALEUR_hote = 'prod.kwk.eu.com';
+    $VALEUR_port = '3306';
+    $VALEUR_nom_bd = 'total-refontedam';
+    $VALEUR_user = 'alaidin';
+    $VALEUR_mot_de_passe = 'alaidin';
+
+    try {
+        $pdo = new PDO('mysql:host='.$VALEUR_hote.';port='.$VALEUR_port.';dbname='.$VALEUR_nom_bd, $VALEUR_user, $VALEUR_mot_de_passe);
+
+        $sql = "SELECT * from restore_files where is_restored <> 1 and width = :width and height=:height and s_format in ('jpg', 'png')";
+
+        $req = $pdo->prepare($sql);
+        $req->bindValue(':width',$width,PDO::PARAM_INT);
+        $req->bindValue(':height',$height,PDO::PARAM_INT);
+        $req->execute();
+
+        $rows = $req->fetchAll(PDO::FETCH_OBJ);
+
+        $nb = 1;
+
+        foreach ($rows as $row) {
+            $ret[] = array(
+                'width' => $row->width,
+                'rfcode' => $row->id,
+                'height' => $row->height,
+                'fname' => $row->fname);
+
+        }
+        return $ret;
+    } catch (PDOException $Exception) {
+        // PHP Fatal Error. Second Argument Has To Be An Integer, But PDOException::getCode Returns A
+        // String.
+        echo $Exception->getMessage().' : '.$Exception->getCode();
+        return false;
+    }
+}
+
 
 function findByPixels($cocode)
 {
@@ -137,9 +176,10 @@ try{
         if (!file_exists('/home/ubuntu/restore/olddir/thumbdir/'.$cocode.'.jpg'))
             continue;
 
-        $rowsRf = findByPixels($cocode);
-        if (count($rowsRf)>0)
-            break;
+//        $rowsRf = findByPixels($cocode);
+//        if (count($rowsRf)>0)
+//            break;
+        $rowRf = findBySizes($rowCo->i_width, $rowCo->i_height);
     }
 
 
