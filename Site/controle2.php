1a9554b51a2e1d75fd9867508bc90c71faed6186
Alexandre Laidin <alexandre@lxi.eu.com>
2017-09-09 15:04:49 +0200

    m

--- Site/controle2.php
+++ Site/controle2.php
@@ -56,7 +56,7 @@ function findBySizes($width, $height)
 }
 
 
-function findByPixels($cocode, $width, $height)
+function findByPixels($cocode)
 {
     $VALEUR_hote = 'prod.kwk.eu.com';
     $VALEUR_port = '3306';
@@ -164,6 +164,114 @@ function findByPixels($cocode, $width, $height)
     }
 }
 
+function findByPixelsSize($cocode, $width, $height)
+{
+    $VALEUR_hote = 'prod.kwk.eu.com';
+    $VALEUR_port = '3306';
+    $VALEUR_nom_bd = 'total-refontedam';
+    $VALEUR_user = 'alaidin';
+    $VALEUR_mot_de_passe = 'alaidin';
+
+
+    $taux = 0.1;
+    $vals[1] = array(
+        'r'=> array('min'=>0,'max'=>0),
+        'g'=> array('min'=>0,'max'=>0),
+        'b'=> array('min'=>0,'max'=>0));
+    $tCols[1] = array('p1_r','p1_g','p1_b');
+    $ret = array();
+
+    for ($i =1; $i <= 10; $i++){
+        $tCols[$i] = array('p'.$i.'_r','p'.$i.'_g','p'.$i.'_b');
+    }
+
+    try {
+        $pdo = new PDO('mysql:host='.$VALEUR_hote.';port='.$VALEUR_port.';dbname='.$VALEUR_nom_bd, $VALEUR_user, $VALEUR_mot_de_passe);
+
+        $sql = "SELECT icode, p1_r, p1_g, p1_b, p1_a, p2_r, p2_g, p2_b, p2_a, p3_r, p3_g, p3_b, p3_a, p4_r, p4_g, p4_b, p4_a, p5_r, p5_g, p5_b, p5_a, p6_r, p6_g, p6_b, p6_a, p7_r, p7_g, p7_b, p7_a, p8_r, p8_g, p8_b, p8_a, p9_r, p9_g, p9_b, p9_a, p10_r, p10_g, p10_b, p10_a
+        FROM restore_ofile_colors where icode = :cocode";
+        $sql .= " and (";
+
+        for ($i =1; $i <= 10; $i++){
+            $sql .= (($i>1)?" + ":"").$tCols[$i][0]." + ".$tCols[$i][1]." + ".$tCols[$i][2];
+        }
+        $sql .= ") <> 7650 and (";
+
+        for ($i =1; $i <= 10; $i++){
+            $sql .= (($i>1)?" + ":"").$tCols[$i][0]." + ".$tCols[$i][1]." + ".$tCols[$i][2];
+        }
+        $sql .= ") <> 0 ";
+
+        $req = $pdo->prepare($sql);
+        $req->bindValue(':cocode',$cocode,PDO::PARAM_INT);
+        $req->execute();
+
+        $rows = $req->fetchAll(PDO::FETCH_OBJ);
+
+        $nb = 1;
+
+        foreach ($rows as $row) {
+            $nb++;
+
+            for ($i =1; $i <= 10; $i++){
+                $rcol = $tCols[$i][0];
+                $rval = $row->$rcol;
+
+                $gcol = $tCols[$i][1];
+                $gval = $row->$gcol;
+
+                $bcol = $tCols[$i][2];
+                $bval = $row->$bcol;
+
+                $vals[$i]['r']['min'] = $rval*(1-$taux);
+                $vals[$i]['r']['max'] = $rval*(1+ $taux);
+                $vals[$i]['g']['min'] = $gval*(1-$taux);
+                $vals[$i]['g']['max'] = $gval*(1+ $taux);
+                $vals[$i]['b']['min'] = $bval*(1-$taux);
+                $vals[$i]['b']['max'] = $bval*(1+ $taux);
+            }
+
+            $sql = "SELECT distinct rfcode, rf.height, rf.width, rf.fname
+                    FROM restore_nfile_colors, restore_files rf where rfcode = rf.id 
+                    and rf.width=:width and rf.height=:height";
+
+            $sql .= " and (";
+
+            for ($i =1; $i <= 10; $i++){
+                $sql .= (($i>1)?" and ":"").$tCols[$i][0]." between ".$vals[$i]['r']['min']." and ".$vals[$i]['r']['max'];
+                $sql .= " and ".$tCols[$i][1]." between ".$vals[$i]['g']['min']." and ".$vals[$i]['g']['max'];
+                $sql .= " and ".$tCols[$i][2]." between ".$vals[$i]['b']['min']." and ".$vals[$i]['b']['max'];
+            }
+
+            $sql .= ")";
+
+            //print_r($sql);
+
+            $reqSel = $pdo->prepare($sql);
+            $reqSel->bindValue(':width',$width,PDO::PARAM_INT);
+            $reqSel->bindValue(':height',$height,PDO::PARAM_INT);
+            $reqSel->execute();
+
+            $selRows = $reqSel->fetchAll(PDO::FETCH_OBJ);
+
+            foreach ($selRows as $selRow) {
+                $ret[] = array(
+                    'rfcode' => $selRow->rfcode,
+                    'width' => $selRow->width,
+                    'height' => $selRow->height,
+                    'fname' => $selRow->fname);
+
+            }
+        }
+        return $ret;
+    } catch (PDOException $Exception) {
+        // PHP Fatal Error. Second Argument Has To Be An Integer, But PDOException::getCode Returns A
+        // String.
+        echo $Exception->getMessage().' : '.$Exception->getCode();
+        return false;
+    }
+}
+
 try{
     $pdo = new PDO('mysql:host='.$VALEUR_hote.';port='.$VALEUR_port.';dbname='.$VALEUR_nom_bd, $VALEUR_user, $VALEUR_mot_de_passe);
 
@@ -188,7 +296,7 @@ try{
         if (!file_exists('/home/ubuntu/restore/olddir/thumbdir/'.$cocode.'.jpg'))
             continue;
 
-        $rowsRf = findByPixels($cocode, $rowCo->i_width, $rowCo->i_height);
+        $rowsRf = findByPixelsSize($cocode, $rowCo->i_width, $rowCo->i_height);
         if (count($rowsRf)>0)
             break;
 
