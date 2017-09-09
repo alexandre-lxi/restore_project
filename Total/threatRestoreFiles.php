fa24a8599714b7955c80f9c56d21bfd67ee0025a
Alexandre Laidin <alexandre@lxi.eu.com>
2017-09-09 20:41:58 +0200

    m

--- Total/threatRestoreFiles.php
+++ Total/threatRestoreFiles.php
@@ -168,6 +168,114 @@ function findByPixels($rfcode)
     }
 }
 
+function findByPixelsSize($cocode, $width, $height)
+{
+    $VALEUR_hote = 'prod.kwk.eu.com';
+    $VALEUR_port = '3306';
+    $VALEUR_nom_bd = 'total-refontedam';
+    $VALEUR_user = 'alaidin';
+    $VALEUR_mot_de_passe = 'alaidin';
+
+
+    $taux = 0.1;
+    $vals[1] = array(
+        'r'=> array('min'=>0,'max'=>0),
+        'g'=> array('min'=>0,'max'=>0),
+        'b'=> array('min'=>0,'max'=>0));
+    $tCols[1] = array('p1_r','p1_g','p1_b');
+    $ret = array();
+
+    for ($i =1; $i <= 10; $i++){
+        $tCols[$i] = array('p'.$i.'_r','p'.$i.'_g','p'.$i.'_b');
+    }
+
+    try {
+        $pdo = new PDO('mysql:host='.$VALEUR_hote.';port='.$VALEUR_port.';dbname='.$VALEUR_nom_bd, $VALEUR_user, $VALEUR_mot_de_passe);
+
+        $sql = "SELECT icode, p1_r, p1_g, p1_b, p1_a, p2_r, p2_g, p2_b, p2_a, p3_r, p3_g, p3_b, p3_a, p4_r, p4_g, p4_b, p4_a, p5_r, p5_g, p5_b, p5_a, p6_r, p6_g, p6_b, p6_a, p7_r, p7_g, p7_b, p7_a, p8_r, p8_g, p8_b, p8_a, p9_r, p9_g, p9_b, p9_a, p10_r, p10_g, p10_b, p10_a
+        FROM restore_ofile_colors where icode = :cocode";
+        $sql .= " and (";
+
+        for ($i =1; $i <= 10; $i++){
+            $sql .= (($i>1)?" + ":"").$tCols[$i][0]." + ".$tCols[$i][1]." + ".$tCols[$i][2];
+        }
+        $sql .= ") <> 7650 and (";
+
+        for ($i =1; $i <= 10; $i++){
+            $sql .= (($i>1)?" + ":"").$tCols[$i][0]." + ".$tCols[$i][1]." + ".$tCols[$i][2];
+        }
+        $sql .= ") <> 0 ";
+
+        $req = $pdo->prepare($sql);
+        $req->bindValue(':cocode',$cocode,PDO::PARAM_INT);
+        $req->execute();
+
+        $rows = $req->fetchAll(PDO::FETCH_OBJ);
+
+        $nb = 1;
+
+        foreach ($rows as $row) {
+            $nb++;
+
+            for ($i =1; $i <= 10; $i++){
+                $rcol = $tCols[$i][0];
+                $rval = $row->$rcol;
+
+                $gcol = $tCols[$i][1];
+                $gval = $row->$gcol;
+
+                $bcol = $tCols[$i][2];
+                $bval = $row->$bcol;
+
+                $vals[$i]['r']['min'] = $rval*(1-$taux);
+                $vals[$i]['r']['max'] = $rval*(1+ $taux);
+                $vals[$i]['g']['min'] = $gval*(1-$taux);
+                $vals[$i]['g']['max'] = $gval*(1+ $taux);
+                $vals[$i]['b']['min'] = $bval*(1-$taux);
+                $vals[$i]['b']['max'] = $bval*(1+ $taux);
+            }
+
+            $sql = "SELECT distinct rfcode, rf.height, rf.width, rf.fname
+                    FROM restore_nfile_colors, restore_files rf where rfcode = rf.id 
+                    and rf.width=:width and rf.height=:height";
+
+            $sql .= " and (";
+
+            for ($i =1; $i <= 10; $i++){
+                $sql .= (($i>1)?" and ":"").$tCols[$i][0]." between ".$vals[$i]['r']['min']." and ".$vals[$i]['r']['max'];
+                $sql .= " and ".$tCols[$i][1]." between ".$vals[$i]['g']['min']." and ".$vals[$i]['g']['max'];
+                $sql .= " and ".$tCols[$i][2]." between ".$vals[$i]['b']['min']." and ".$vals[$i]['b']['max'];
+            }
+
+            $sql .= ")";
+
+            //print_r($sql);
+
+            $reqSel = $pdo->prepare($sql);
+            $reqSel->bindValue(':width',$width,PDO::PARAM_INT);
+            $reqSel->bindValue(':height',$height,PDO::PARAM_INT);
+            $reqSel->execute();
+
+            $selRows = $reqSel->fetchAll(PDO::FETCH_OBJ);
+
+            foreach ($selRows as $selRow) {
+                $ret[] = array(
+                    'rfcode' => $selRow->rfcode,
+                    'width' => $selRow->width,
+                    'height' => $selRow->height,
+                    'fname' => $selRow->fname);
+
+            }
+        }
+        return $ret;
+    } catch (PDOException $Exception) {
+        // PHP Fatal Error. Second Argument Has To Be An Integer, But PDOException::getCode Returns A
+        // String.
+        echo $Exception->getMessage().' : '.$Exception->getCode();
+        return false;
+    }
+}
+
 function controlPixels($rfcode, $cocode)
 {
     $VALEUR_hote = 'prod.kwk.eu.com';
@@ -336,9 +444,11 @@ function threatImage()
 
         $sql = "SELECT *
         FROM restore_files
-        WHERE id NOT IN (SELECT rf_code FROM restore_file_co2)
-        AND s_format IN ('psd', 'tif', 'jpg', 'jpeg', 'png', 'gif', 'eps',  'ai')
+        WHERE id NOT IN (SELECT rf_code FROM restore_file_co2)        
+        AND s_format IN ('jpg', 'png')
         and id between 155660 and 206770";
+
+        //AND s_format IN ('psd', 'tif', 'jpg', 'jpeg', 'png', 'gif', 'eps',  'ai')
 
         $req = $pdo->prepare($sql);
         $req->execute();
@@ -849,11 +959,57 @@ function threatImageAn(){
     }
 }
 
+function threatImageByCo()
+{
+    $VALEUR_hote = 'prod.kwk.eu.com';
+    $VALEUR_port = '3306';
+    $VALEUR_nom_bd = 'total-refontedam';
+    $VALEUR_user = 'alaidin';
+    $VALEUR_mot_de_passe = 'alaidin';
+
+    try {
+        $pdo = new PDO('mysql:host='.$VALEUR_hote.';port='.$VALEUR_port.';dbname='.$VALEUR_nom_bd, $VALEUR_user, $VALEUR_mot_de_passe);
+
+        $sql = "select co.i_autocode, imf.i_width, imf.i_height
+            from container co, image_file imf
+            where co.i_autocode not in (select co_code from restore_file_co where is_restored=1)
+            and co.i_autocode not in (SELECT co_code from restore_file_co2)
+            and co.i_autocode not in (select co_code from restore_file_co3)
+            and b_isintrash =0
+            and imf.i_foreigncode = co.i_autocode
+            and imf.s_fileformat in ('.jpg','.png') ";
+
+        //AND s_format IN ('psd', 'tif', 'jpg', 'jpeg', 'png', 'gif', 'eps',  'ai')
+
+        $req = $pdo->prepare($sql);
+        $req->execute();
+
+        $rows = $req->fetchAll(PDO::FETCH_OBJ);
+        $nb = 0;
+
+        foreach ($rows as $row) {
+            echo $row->i_autocode."\n";
+
+            $ret = findByPixelsSize($row->i_autocode, $row->i_width, $row->i_height);
+
+            if (count($ret)==1){
+                echo "    1 row OK";
+                insertCo($ret['rfcode'], $row->i_autocode );
+            }
+        }
+    } catch (PDOException $Exception) {
+        // PHP Fatal Error. Second Argument Has To Be An Integer, But PDOException::getCode Returns A
+        // String.
+        echo $Exception->getMessage().' : '.$Exception->getCode();
+    }
+}
+
 
 //threatImage();
+threatImageByCo();
 //print_r(findByPixels(773));
 //threatOffice();
-threatOfficeAn();
+//threatOfficeAn();
 //threatVideo();
 //threatOthers();
 //threatImageAn();
