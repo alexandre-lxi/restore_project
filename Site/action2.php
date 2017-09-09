2c4ccc2b771b27d84509ec188c74fbabfb1381c5
Alexandre Laidin <alexandre@lxi.eu.com>
2017-09-09 12:42:45 +0200

    m

---
+++ Site/action2.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 07/09/17
+ * Time: 21:00
+ */
+
+$VALEUR_hote = 'prod.kwk.eu.com';
+$VALEUR_port = '3306';
+$VALEUR_nom_bd = 'total-refontedam';
+$VALEUR_user = 'alaidin';
+$VALEUR_mot_de_passe = 'alaidin';
+
+$name = (isset($_POST['name']))?$_POST['name']:'';
+$cocode = (isset($_POST['cocode']))?$_POST['cocode']:'';
+$rfcode = (isset($_POST['list']))?$_POST['list']:'';
+$ttrfcode=  (isset($_POST['ttrfcode']))?$_POST['ttrfcode']:'';
+
+echo "name:". $name.'<br>';
+echo 'cocode:'.$cocode.'<br>';
+echo 'rfcode:'.$rfcode.'<br>';
+
+
+if ($name == ''){
+    $error = 'Vous devez vous identifier !';
+
+    header('Location: http://verif.iris-solutions.fr/controle.php?errors='.$error);
+    exit();
+}
+
+
+try{
+    $pdo = new PDO('mysql:host='.$VALEUR_hote.';port='.$VALEUR_port.';dbname='.$VALEUR_nom_bd, $VALEUR_user, $VALEUR_mot_de_passe);
+
+    $pdo->beginTransaction();
+
+    $sql = "update restore_activity set click=click+1, fusion=fusion+:nb where name=:idname";
+    $reqRA = $pdo->prepare($sql);
+    $reqRA->bindValue(':idname', $name, PDO::PARAM_STR);
+    $reqRA->bindValue(':nb', ($rfcode=='')?0:1,PDO::PARAM_INT);
+    $reqRA->execute();
+
+    if ($rfcode <> '') {
+        $sql = "INSERT INTO restore_file_co3 VALUES (:rfcode, :cocode, FALSE, :who)";
+        $reqrf3 = $pdo->prepare($sql);
+        $reqrf3->bindValue(':rfcode', $rfcode, PDO::PARAM_INT);
+        $reqrf3->bindValue(':cocode', $cocode, PDO::PARAM_INT);
+        $reqrf3->bindValue(':who', $name, PDO::PARAM_INT);
+        $reqrf3->execute();
+
+        $sql = "update restore_files set to_restore=1 where id=:rfcode";
+        $reqRF = $pdo->prepare($sql);
+        $reqRF->bindValue(':rfcode',$rfcode,PDO::PARAM_INT);
+        $reqRF->execute();
+
+        $sql = "update restore_file_co_analyse2 set to_restore=1 where id=:rfcode and co_code=:cocode";
+        $reqRF = $pdo->prepare($sql);
+        $reqRF->bindValue(':rfcode',$rfcode,PDO::PARAM_INT);
+        $reqRF->bindValue(':cocode',$cocode,PDO::PARAM_INT);
+        $reqRF->execute();
+    }else{
+        $sql = "update restore_file_co_analyse2 set to_restore=2 where rf_code=:rfcode  and co_code=:cocode";
+        $reqRF = $pdo->prepare($sql);
+        $reqRF->bindValue(':rfcode',$ttrfcode,PDO::PARAM_INT);
+        $reqRF->bindValue(':cocode',$cocode,PDO::PARAM_INT);
+        $reqRF->execute();
+    }
+
+
+    $pdo->commit();
+    header('Location: http://verif.iris-solutions.fr/controle.php?name='.$name);
+
+} catch (PDOException $Exception) {
+    // PHP Fatal Error. Second Argument Has To Be An Integer, But PDOException::getCode Returns A
+    // String.
+    $pdo->rollBack();
+    $error = $Exception->getMessage().' : '.$Exception->getCode();
+    header('Location: http://verif.iris-solutions.fr/controle.php?errors='.$error);
+    exit();
+}
--- Site/controle2.php
+++ Site/controle2.php
@@ -8,22 +8,122 @@ $VALEUR_mot_de_passe = 'alaidin';
 
 $name = (isset($_GET['name'])?$_GET['name']:'');
 
+
+function findByPixels($cocode)
+{
+    $VALEUR_hote = 'prod.kwk.eu.com';
+    $VALEUR_port = '3306';
+    $VALEUR_nom_bd = 'total-refontedam';
+    $VALEUR_user = 'alaidin';
+    $VALEUR_mot_de_passe = 'alaidin';
+
+
+    $taux = 0.1;
+    $vals[1] = array(
+        'r'=> array('min'=>0,'max'=>0),
+        'g'=> array('min'=>0,'max'=>0),
+        'b'=> array('min'=>0,'max'=>0));
+    $tCols[1] = array('p1_r','p1_g','p1_b');
+    $ret = array();
+
+    for ($i =1; $i <= 10; $i++){
+        $tCols[$i] = array('p'.$i.'_r','p'.$i.'_g','p'.$i.'_b');
+    }
+
+    try {
+        $pdo = new PDO('mysql:host='.$VALEUR_hote.';port='.$VALEUR_port.';dbname='.$VALEUR_nom_bd, $VALEUR_user, $VALEUR_mot_de_passe);
+
+        $sql = "SELECT icode, p1_r, p1_g, p1_b, p1_a, p2_r, p2_g, p2_b, p2_a, p3_r, p3_g, p3_b, p3_a, p4_r, p4_g, p4_b, p4_a, p5_r, p5_g, p5_b, p5_a, p6_r, p6_g, p6_b, p6_a, p7_r, p7_g, p7_b, p7_a, p8_r, p8_g, p8_b, p8_a, p9_r, p9_g, p9_b, p9_a, p10_r, p10_g, p10_b, p10_a
+        FROM restore_ofile_colors where icode = :cocode";
+        $sql .= " and (";
+
+        for ($i =1; $i <= 10; $i++){
+            $sql .= (($i>1)?" + ":"").$tCols[$i][0]." + ".$tCols[$i][1]." + ".$tCols[$i][2];
+        }
+        $sql .= ") <> 7650 and (";
+
+        for ($i =1; $i <= 10; $i++){
+            $sql .= (($i>1)?" + ":"").$tCols[$i][0]." + ".$tCols[$i][1]." + ".$tCols[$i][2];
+        }
+        $sql .= ") <> 0 ";
+
+        $req = $pdo->prepare($sql);
+        $req->bindValue(':cocode',$cocode,PDO::PARAM_INT);
+        $req->execute();
+
+        $rows = $req->fetchAll(PDO::FETCH_OBJ);
+
+        $nb = 1;
+
+        foreach ($rows as $row) {
+            $nb++;
+
+            for ($i =1; $i <= 10; $i++){
+                $rcol = $tCols[$i][0];
+                $rval = $row->$rcol;
+
+                $gcol = $tCols[$i][1];
+                $gval = $row->$gcol;
+
+                $bcol = $tCols[$i][2];
+                $bval = $row->$bcol;
+
+                $vals[$i]['r']['min'] = $rval*(1-$taux);
+                $vals[$i]['r']['max'] = $rval*(1+ $taux);
+                $vals[$i]['g']['min'] = $gval*(1-$taux);
+                $vals[$i]['g']['max'] = $gval*(1+ $taux);
+                $vals[$i]['b']['min'] = $bval*(1-$taux);
+                $vals[$i]['b']['max'] = $bval*(1+ $taux);
+            }
+
+            $sql = "SELECT distinct rfcode, rf.height, rf.width, rf.fname
+                    FROM restore_nfile_colors, restore_files rf where rfcode = rf.id";
+
+            $sql .= " and (";
+
+            for ($i =1; $i <= 10; $i++){
+                $sql .= (($i>1)?" and ":"").$tCols[$i][0]." between ".$vals[$i]['r']['min']." and ".$vals[$i]['r']['max'];
+                $sql .= " and ".$tCols[$i][1]." between ".$vals[$i]['g']['min']." and ".$vals[$i]['g']['max'];
+                $sql .= " and ".$tCols[$i][2]." between ".$vals[$i]['b']['min']." and ".$vals[$i]['b']['max'];
+            }
+
+            $sql .= ")";
+
+            //print_r($sql);
+
+            $reqSel = $pdo->prepare($sql);
+            $reqSel->execute();
+
+            $selRows = $reqSel->fetchAll(PDO::FETCH_OBJ);
+
+            foreach ($selRows as $selRow) {
+                $ret[] = array(
+                        'rfcode' => $selRow->rfcode,
+                        'widht' => $selRow->width,
+                        'height' => $selRow->heigth,
+                        'fname' => $selRow->fname);
+
+            }
+        }
+        return $ret;
+    } catch (PDOException $Exception) {
+        // PHP Fatal Error. Second Argument Has To Be An Integer, But PDOException::getCode Returns A
+        // String.
+        echo $Exception->getMessage().' : '.$Exception->getCode();
+        return false;
+    }
+}
+
 try{
     $pdo = new PDO('mysql:host='.$VALEUR_hote.';port='.$VALEUR_port.';dbname='.$VALEUR_nom_bd, $VALEUR_user, $VALEUR_mot_de_passe);
 
-    $sql = "select co2.co_code, imf.i_width, imf.i_height, rf.id
-            from restore_file_co_analyse2 co2, restore_files rf, image_file imf
-            where co2.rf_code = rf.id
-            and rf.s_format in ('jpg','png')            
-            and rf.is_restored = 0
-            and rf.to_restore=0
-            and (co2.to_restore = 0 or co2.to_restore is null) 
-            and co2.co_code <> 1
-            and i_foreigncode = co2.co_code
-            and co2.co_code in (select i_autocode from container where b_isintrash <> 0)
-            order by RAND()
+    $sql = "select co.i_autocode, imf.i_width, imf.i_height
+            from container co, image_file imf
+            where co.i_autocode not in (select co_code from restore_file_co where is_restored=1)
+            and co.i_autocode not in (SELECT co_code from restore_file_co2)
+            and imf.i_foreigncode = co.i_autocode
+            and imf.s_fileformat in ('.jpg','.png');
             limit 1
-            
             ";
 
     $req = $pdo->prepare($sql);
@@ -31,19 +131,9 @@ try{
 
     $rows = $req->fetchAll(PDO::FETCH_OBJ);
     $rowCo = $rows[0];
-    $cocode = $rowCo->co_code;
+    $cocode = $rowCo->i_autocode;
 
-
-    $sql = "select rf_code, fname, width, height
-            from restore_file_co_analyse2, restore_files
-            where co_code = :cocode            
-            and id = rf_code";
-
-    $req = $pdo->prepare($sql);
-    $req->bindValue(':cocode', $rowCo->co_code, PDO::PARAM_INT);
-    $req->execute();
-
-    $rowsRf = $req->fetchAll(PDO::FETCH_OBJ);
+    $rowsRf = findByPixels($cocode);
 
 } catch (PDOException $Exception) {
     // PHP Fatal Error. Second Argument Has To Be An Integer, But PDOException::getCode Returns A
@@ -67,10 +157,9 @@ if (isset($_GET['error'])){
     echo '<p style="color: red;">'.$_GET['error'].'</p>';
 }else{
     ?>
-    <form action="action.php" method="post">
+    <form action="action2.php" method="post">
         <input type="hidden" name="cocode" value="<?php echo $cocode; ?>">
-        <input type="hidden" name="ttrfcode" value="<?php echo $rowCo->id; ?>">
-
+        
         <div id="entete">
             <p>Votre nom :
                 <select name="name">
@@ -99,7 +188,7 @@ if (isset($_GET['error'])){
                 <ul class="ul">
                     <?php
                     foreach ($rowsRf as $rowRf) {
-                        $fname = basename($rowRf->fname);
+                        $fname = basename($rowRf['fname']);
                         $fname = explode('.',$fname);
                         $fname = 'pictures/tmpdir/'.$fname[0].'.jpg';
 
@@ -108,10 +197,10 @@ if (isset($_GET['error'])){
                         echo '<tr>';
                         echo '<td>
                                     <img style="margin: 5px" src="'.$fname.'"\>
-                                    <p>Width: '.$rowRf->width.'</p>
-                                    <p>Height: '.$rowRf->height.'</p>
+                                    <p>Width: '.$rowRf['width'].'</p>
+                                    <p>Height: '.$rowRf['height'].'</p>
                               </td>';
-                        echo '<td><input type="radio" name="list" value="'.$rowRf->rf_code.'"></td>';
+                        echo '<td><input type="radio" name="list" value="'.$rowRf['rf_code'].'"></td>';
                         echo '</tr>';
                         echo '</table>';
                         echo '</li>';
