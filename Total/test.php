139d27599f2c3ff33f66f3421a7a5cb3e5b24764
Alexandre Laidin <alexandre@lxi.eu.com>
2017-09-23 17:53:51 +0200

    m

--- Total/test.php
+++ Total/test.php
@@ -7,8 +7,41 @@
  */
 
 
-$img = new Imagick();
-//$img->readImage('/home/ubuntu/restore/toAnalyse/recup_dir.91/f275398656.psd');
-$img->readImage('/home/ubuntu/restore/toAnalyse/recup_dir.9/f7503872.psd');
-echo $img->getNumberImages();
-$img->clear();
+$VALEUR_hote = '127.0.0.1';
+$VALEUR_port = '3306';
+$VALEUR_nom_bd = 'total-refontedam';
+$VALEUR_user = 'alaidin';
+$VALEUR_mot_de_passe = 'alaidin';
+
+$ffmpeg_path = 'ffmpeg'; //or: /usr/bin/ffmpeg , or /usr/local/bin/ffmpeg - depends on your installation (type which ffmpeg into a console to find the install path)
+$vid = 'PATH/TO/VIDEO'; //Replace here!
+
+try {
+    $pdo = new PDO('mysql:host='.$VALEUR_hote.';port='.$VALEUR_port.';dbname='.$VALEUR_nom_bd, $VALEUR_user, $VALEUR_mot_de_passe);
+
+    $sql = "SELECT *
+      FROM restore_files
+      WHERE s_format in ('psd')                 
+      and width = 0
+      ";
+
+    $req = $pdo->prepare($sql);
+    $req->execute();
+
+    $rows = $req->fetchAll(PDO::FETCH_OBJ);
+
+    $nb = 0;
+
+    foreach ($rows as $row) {
+        $oldFile = str_replace('/home/ubuntu/restore/toAnalyse/', $dirsource, $row->fname);
+        $newFile = '/var/www/tmp/toco/'.basename($oldFile);
+
+        copy($oldFile, $newFile);
+
+    }
+
+    } catch (PDOException $Exception) {
+    // PHP Fatal Error. Second Argument Has To Be An Integer, But PDOException::getCode Returns A
+    // String.
+    echo $Exception->getMessage().' : '.$Exception->getCode();
+}
