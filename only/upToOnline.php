8c94def2ddf85043cea9618abe2de667f24f4f2a
Alexandre Laidin <alexandre@lxi.eu.com>
2018-04-27 17:25:51 +0200

    mm

--- only/upToOnline.php
+++ only/upToOnline.php
@@ -28,34 +28,34 @@ function testFile($file)
     try {
         $pdo = new PDO('mysql:host='.$VALEUR_hote.';port='.$VALEUR_port.';dbname='.$VALEUR_nom_bd, $VALEUR_user, $VALEUR_mot_de_passe);
 
-//        $sql = "insert into onlyfrance.restore_files (fname, isOldFile) values(:fname, TRUE )";
-//        $rqt = $pdo->prepare($sql);
-//        $rqt->bindValue(':fname', $fname, PDO::PARAM_STR);
-//        $rqt->execute();
-//
-//        $sql = "select max(id) id from onlyfrance.restore_files";
-//        $rqt = $pdo->prepare($sql);
-//        $rqt->execute();
-//        $id = $rqt->fetchAll(PDO::FETCH_OBJ);
-//        $id = $id[0]->id;
-//
-//        $sql = "insert into onlyfrance.restore_file_co (rf_code, co_code) values (:rfcode, :cocode) ";
-//        $rqt = $pdo->prepare($sql);
-//        $rqt->bindValue(':rfcode', $id, PDO::PARAM_INT);
-//        $rqt->bindValue(':cocode', $cocode, PDO::PARAM_INT);
-//        $rqt->execute();
-//
-//        $sql = "update onlyfrance.container co
-//                        inner join onlyfrance.restore_file_co rfc on co.i_autocode = rfc.co_code
-//                        inner join onlyfrance.restore_container_sav cos on cos.i_autocode = rfc.co_code
-//                            set co.b_isonline = 1
-//                        where co.b_isonline =0
-//                        and cos.b_isonline =1
-//                        and rfc.co_code = :cocode";
-//
-//        $rqt = $pdo->prepare($sql);
-//        $rqt->bindValue(':cocode', $cocode, PDO::PARAM_INT);
-//        $rqt->execute();
+        $sql = "insert into onlyfrance.restore_files (fname, isOldFile) values(:fname, TRUE )";
+        $rqt = $pdo->prepare($sql);
+        $rqt->bindValue(':fname', $fname, PDO::PARAM_STR);
+        $rqt->execute();
+
+        $sql = "select max(id) id from onlyfrance.restore_files";
+        $rqt = $pdo->prepare($sql);
+        $rqt->execute();
+        $id = $rqt->fetchAll(PDO::FETCH_OBJ);
+        $id = $id[0]->id;
+
+        $sql = "insert into onlyfrance.restore_file_co (rf_code, co_code) values (:rfcode, :cocode) ";
+        $rqt = $pdo->prepare($sql);
+        $rqt->bindValue(':rfcode', $id, PDO::PARAM_INT);
+        $rqt->bindValue(':cocode', $cocode, PDO::PARAM_INT);
+        $rqt->execute();
+
+        $sql = "update onlyfrance.container co
+                        inner join onlyfrance.restore_file_co rfc on co.i_autocode = rfc.co_code
+                        inner join onlyfrance.restore_container_sav cos on cos.i_autocode = rfc.co_code
+                            set co.b_isonline = 1
+                        where co.b_isonline =0
+                        and cos.b_isonline =1
+                        and rfc.co_code = :cocode";
+
+        $rqt = $pdo->prepare($sql);
+        $rqt->bindValue(':cocode', $cocode, PDO::PARAM_INT);
+        $rqt->execute();
 
         echo "      Update: ".date("H:i:s", microtime(true)- $timestart)."\n";
 
