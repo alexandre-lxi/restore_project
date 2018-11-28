80fb2abfd3fb64da6952ad945da340746a63fda1
Alexandre Laidin <alexandre@lxi.eu.com>
2018-11-28 11:23:02 +0100

    NN

--- Total/repair_blob.php
+++ Total/repair_blob.php
@@ -77,12 +77,14 @@ function threat()
 					copy("/var/www/projects/total-1410-refontedam/back/ico/pptx.jpg", $webdir.$id.'.jpg');
 					copy("/var/www/projects/total-1410-refontedam/back/ico/pptx.jpg", $thumbdir.$id.'.jpg');
 				}elseif($fileext == 'pdf'){
-					$convert = "/var/www/utils/nconvert/nconvert -out jpeg -o \"".$webdir.$id.'.jpg'."\" -ratio -resize 640 640  \"".$newfname."\"";
+					$convert = "sudo /var/www/utils/nconvert/nconvert -out jpeg -o \"".$tmpdir.$id.'.jpg'."\" -ratio -resize 640 640  \"".$newfname."\"";
 					print("\t\t".'Convert : '.$convert."\n");
 					exec($convert);
-					$convert = "/var/www/utils/nconvert/nconvert -out jpeg -o \"".$thumbdir.$id.'.jpg'."\" -ratio -resize 280 280  \"".$newfname."\"";
+					copy($tmpdir.$id.'.jpg', $webdir.$id.'.jpg');
+					$convert = "/var/www/utils/nconvert/nconvert -out jpeg -o \"".$tmpdir.$id.'.jpg'."\" -ratio -resize 280 280  \"".$newfname."\"";
 					print("\t\t".'Convert : '.$convert."\n");
 					exec($convert);
+					copy($tmpdir.$id.'.jpg', $thumbdir.$id.'.jpg');
 				}
 				else{
 						$convert = 'convert '.$newfname.' -resize 640x640 -quality 95 '.$webdir.$id.'.jpg';
