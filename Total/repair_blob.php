ccf1bc6d250787912b1d3cc2f58619a875e989c2
Alexandre Laidin <alexandre@lxi.eu.com>
2018-11-29 11:34:52 +0100

    NN

--- Total/repair_blob.php
+++ Total/repair_blob.php
@@ -1,47 +1,6 @@
 <?php
 
-function get_pdf_prop($file)
-{
-	$f = fopen($file,'rb');
-	if(!$f)
-		return false;
-
-	//Read the last 16KB
-	fseek($f, -16384, SEEK_END);
-	$s = fread($f, 16384);
-
-	//Extract cross-reference table and trailer
-	if(!preg_match("/xref[\r\n]+(.*)trailer(.*)startxref/s", $s, $a))
-		return false;
-	$xref = $a[1];
-	$trailer = $a[2];
-
-	//Extract Info object number
-	if(!preg_match('/Info ([0-9]+) /', $trailer, $a))
-		return false;
-	$object_no = $a[1];
-
-	//Extract Info object offset
-	$lines = preg_split("/[\r\n]+/", $xref);
-	$line = $lines[1 + $object_no];
-	$offset = (int)$line;
-	if($offset == 0)
-		return false;
-
-	//Read Info object
-	fseek($f, $offset, SEEK_SET);
-	$s = fread($f, 1024);
-	fclose($f);
-
-	//Extract properties
-	if(!preg_match('/<<(.*)>>/Us', $s, $a))
-		return false;
-	$n = preg_match_all('|/([a-z]+) ?\((.*)\)|Ui', $a[1], $a);
-	$prop = array();
-	for($i=0; $i<$n; $i++)
-		$prop[$a[1][$i]] = $a[2][$i];
-	return $prop;
-}
+include 'PDFInfo.php';
 
 function threat()
 {
@@ -100,9 +59,9 @@ function threat()
 				print("\t".'Convert file: '.$newfname."\n");
 
 
-				$stringedPDF = get_pdf_prop($newfname);
+				$pdf = new PDFInfo($newfname);
 
-				var_dump($stringedPDF);
+				var_dump($pdf);
 
 
 //				if ($fileext == 'mp4'){
