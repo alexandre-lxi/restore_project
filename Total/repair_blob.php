4a6c12ad3084bd31582934c6d356ca7866074c3c
Alexandre Laidin <alexandre@lxi.eu.com>
2018-11-29 12:24:05 +0100

    NN

--- Total/repair_blob.php
+++ Total/repair_blob.php
@@ -22,12 +22,9 @@ function threat()
 
 		$sql = "select co.i_autocode id, co.*, cq.*
 		from container co
-				join image_infofr ifr on ifr.i_foreigncode = co.i_autocode
-	join conversion_queue cq on cq.i_containercode = co.i_autocode
-where ifr.s_objectname = 'blob'
-and co.dt_created > '2018-10-01'
-and co.s_reference <> 'blob'
-and co.i_autocode <> 70423
+          join conversion_queue cq on cq.i_containercode = co.i_autocode
+				where s_reference = 'blob'
+				and dt_created > '2018-10-01'
 				";
 
 
@@ -68,81 +65,78 @@ and co.i_autocode <> 70423
 
 				if ($fileext == 'mp4'){
 
-//					$convert 	= "ffmpeg -i \"".$newfname."\" -ss 10 -vframes 1 -s 1920x1080 \"".$tmpdir.$id.'.jpg'."\"";
-//					print("\t\t".'Convert : '.$convert."\n");
-//					exec($convert);
-//
-//					$convert 	= 'convert '.$tmpdir.$id.'.jpg'.' -resize 640x640 -quality 95 '.$webdir.$id.'.jpg';
-//					print("\t\t".'Convert : '.$convert."\n");
-//					exec($convert);
-//
-//					$convert 	= 'convert '.$tmpdir.$id.'.jpg'.' -resize 280x280 -quality 95 '.$thumbdir.$id.'.jpg';
-//					print("\t\t".'Convert : '.$convert."\n");
-//					exec($convert);
+					$convert 	= "ffmpeg -i \"".$newfname."\" -ss 10 -vframes 1 -s 1920x1080 \"".$tmpdir.$id.'.jpg'."\"";
+					print("\t\t".'Convert : '.$convert."\n");
+					exec($convert);
+
+					$convert 	= 'convert '.$tmpdir.$id.'.jpg'.' -resize 640x640 -quality 95 '.$webdir.$id.'.jpg';
+					print("\t\t".'Convert : '.$convert."\n");
+					exec($convert);
+
+					$convert 	= 'convert '.$tmpdir.$id.'.jpg'.' -resize 280x280 -quality 95 '.$thumbdir.$id.'.jpg';
+					print("\t\t".'Convert : '.$convert."\n");
+					exec($convert);
 
 					$title = $ref;
 
 
 				}elseif($fileext == 'zip') {
-//					copy("/var/www/projects/total-1410-refontedam/back/ico/zip.jpg", $webdir.$id.'.jpg');
-//					copy("/var/www/projects/total-1410-refontedam/back/ico/zip.jpg", $thumbdir.$id.'.jpg');
+					copy("/var/www/projects/total-1410-refontedam/back/ico/zip.jpg", $webdir.$id.'.jpg');
+					copy("/var/www/projects/total-1410-refontedam/back/ico/zip.jpg", $thumbdir.$id.'.jpg');
 					$title = $ref;
 				}
 				elseif($fileext == 'pptx') {
-//					copy("/var/www/projects/total-1410-refontedam/back/ico/pptx.jpg", $webdir.$id.'.jpg');
-//					copy("/var/www/projects/total-1410-refontedam/back/ico/pptx.jpg", $thumbdir.$id.'.jpg');
+					copy("/var/www/projects/total-1410-refontedam/back/ico/pptx.jpg", $webdir.$id.'.jpg');
+					copy("/var/www/projects/total-1410-refontedam/back/ico/pptx.jpg", $thumbdir.$id.'.jpg');
 
 					$title = $ref;
 				}elseif($fileext == 'pdf'){
-//					$convert = "sudo /var/www/utils/nconvert/nconvert -out jpeg -o \"".$tmpdir.$id.'.jpg'."\" -ratio -resize 640 640  \"".$newfname."\"";
-//					print("\t\t".'Convert : '.$convert."\n");
-//					exec($convert);
-//					rename($tmpdir.$id.'.jpg', $webdir.$id.'.jpg');
-//					$convert = "/var/www/utils/nconvert/nconvert -out jpeg -o \"".$tmpdir.$id.'.jpg'."\" -ratio -resize 280 280  \"".$newfname."\"";
-//					print("\t\t".'Convert : '.$convert."\n");
-//					exec($convert);
-//					rename($tmpdir.$id.'.jpg', $thumbdir.$id.'.jpg');
+					$convert = "sudo /var/www/utils/nconvert/nconvert -out jpeg -o \"".$tmpdir.$id.'.jpg'."\" -ratio -resize 640 640  \"".$newfname."\"";
+					print("\t\t".'Convert : '.$convert."\n");
+					exec($convert);
+					rename($tmpdir.$id.'.jpg', $webdir.$id.'.jpg');
+					$convert = "/var/www/utils/nconvert/nconvert -out jpeg -o \"".$tmpdir.$id.'.jpg'."\" -ratio -resize 280 280  \"".$newfname."\"";
+					print("\t\t".'Convert : '.$convert."\n");
+					exec($convert);
+					rename($tmpdir.$id.'.jpg', $thumbdir.$id.'.jpg');
 
 					$pdf = new PDFInfo($newfname);
 					$title = $pdf->title;
 
 				}
 				else{
-//						$convert = 'convert '.$newfname.' -resize 640x640 -quality 95 '.$webdir.$id.'.jpg';
-//						print("\t\t".'Convert : '.$convert."\n");
-//						exec($convert);
-//
-//
-//						$convert = 'convert '.$newfname.' -resize 280x280 -quality 95 '.$thumbdir.$id.'.jpg';
-//						print("\t\t".'Convert : '.$convert."\n");
-//						exec($convert);
+						$convert = 'convert '.$newfname.' -resize 640x640 -quality 95 '.$webdir.$id.'.jpg';
+						print("\t\t".'Convert : '.$convert."\n");
+						exec($convert);
+
+
+						$convert = 'convert '.$newfname.' -resize 280x280 -quality 95 '.$thumbdir.$id.'.jpg';
+						print("\t\t".'Convert : '.$convert."\n");
+						exec($convert);
 
 					$getID3 = new getID3();
 					$fileinfo = $getID3->analyze($newfname);
 					var_dump($fileinfo);
 					}
-//
-//
-//
-				print("\t".'Update Database file: '.$ref."\n");
-//				$sql = "update container set s_reference = :ref where i_autocode = :code";
-//
-//				$ins = $pdo->prepare($sql);
-//				$ins->bindValue(':code', $cont->id, PDO::PARAM_INT);
-//				$ins->bindValue(':ref', $ref, PDO::PARAM_STR);
-//				$ins->execute();
-//
-//				$sql = "update image_file set s_path = :fpath, s_filename = :fname, s_fileformat = :ext where i_foreigncode = :code";
-//
-//				$ins = $pdo->prepare($sql);
-//				$ins->bindValue(':code', $cont->id, PDO::PARAM_INT);
-//				$ins->bindValue(':fpath', $newfname, PDO::PARAM_STR);
-//				$ins->bindValue(':fname', $ref, PDO::PARAM_STR);
-//				$ins->bindValue(':ext', '.'.$fileext, PDO::PARAM_STR);
-//				$ins->execute();
-
 
 				if ($title !== false){
+					print("\t".'Update Database file: '.$ref."\n");
+					$sql = "update container set s_reference = :ref where i_autocode = :code";
+
+					$ins = $pdo->prepare($sql);
+					$ins->bindValue(':code', $cont->id, PDO::PARAM_INT);
+					$ins->bindValue(':ref', $ref, PDO::PARAM_STR);
+					$ins->execute();
+
+					$sql = "update image_file set s_path = :fpath, s_filename = :fname, s_fileformat = :ext where i_foreigncode = :code";
+
+					$ins = $pdo->prepare($sql);
+					$ins->bindValue(':code', $cont->id, PDO::PARAM_INT);
+					$ins->bindValue(':fpath', $newfname, PDO::PARAM_STR);
+					$ins->bindValue(':fname', $ref, PDO::PARAM_STR);
+					$ins->bindValue(':ext', '.'.$fileext, PDO::PARAM_STR);
+					$ins->execute();
+
 					$sql = "update image_infofr set s_objectname = :title where i_foreigncode = :code";
 
 					$ins = $pdo->prepare($sql);
